6a9045eaece4fe1c220310e8e112ae4f8eeb5f6d
LucasAlejandro <alejandrolucas9546@gmail.com>
2018-12-05 13:43:53 +0100

    ejemplos todo

---
+++ mapsAPI/Controllers/GeoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mapsAPI.Controllers
+{
+    public class GeoController : Controller
+    {
+        // GET: Geo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Autocomplete()
+        {
+            return View();
+        }
+
+    }
+}
--- mapsAPI/Controllers/HomeController.cs
+++ mapsAPI/Controllers/HomeController.cs
@@ -26,5 +26,7 @@ namespace mapsAPI.Controllers
 
             return View();
         }
-    }
+       
+  }
+
 }
